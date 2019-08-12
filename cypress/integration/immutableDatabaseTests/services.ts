6ed2cc9d57e80a59fc07be76678547c80be4672f
garyluu <garylau.work@gmail.com>
2019-08-12 15:55:38 -0400

    Another fix

--- cypress/integration/immutableDatabaseTests/services.ts
+++ cypress/integration/immutableDatabaseTests/services.ts
@@ -93,6 +93,7 @@ describe('Dockstore Home', () => {
           response: json
         });
       });
+      cy.get('mat-expansion-panel').should('have.length', 1);
       cy.get('.mat-expansion-indicator').first().click();
       cy.get('[data-cy=sync-with-github-org]').first().click();
       cy.get('mat-expansion-panel').should('have.length', 1);
