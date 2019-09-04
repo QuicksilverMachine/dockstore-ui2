830cc875be8314b2ca11980b04dde3c70ded274d
garyluu <garylau.work@gmail.com>
2019-09-04 14:51:39 -0400

    Limit more

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       environment: 'dev'
     },
     parallelOptions: {
-      executors: 6
+      executors: 3
     },
     reporters: ['kjhtml', 'spec'],
     port: 9876,
