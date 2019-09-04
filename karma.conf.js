db0c1cb60b11feca7140cb649b11ce6a9bb3755a
garyluu <garylau.work@gmail.com>
2019-09-04 15:01:54 -0400

    2 executors

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       environment: 'dev'
     },
     parallelOptions: {
-      executors: 3
+      executors: 2
     },
     reporters: ['kjhtml', 'spec'],
     port: 9876,
